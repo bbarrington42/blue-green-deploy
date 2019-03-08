64648b1b8b2f8736470344def8e11650a5f5b30f
Bill Barrington <bill.barrington@candidpartners.com>
2019-03-08 09:07:15 -0500

    Added registerInstances.

--- index.js
+++ index.js
@@ -10,20 +10,25 @@ const classic_elb = new ELB({
     region: 'us-east-1'
 });
 
+const dev = 'cda-dispe-ElasticL-1N0VM8V2BCD3V';
+const prod_blue = 'cda-dispe-ElasticL-1TPFRAVFTJUDW';
+const prod_green = 'cda-dispe-ElasticL-1XM2UQU1NXKT6';
 
-// cda-dispe-ElasticL-1N0VM8V2BCD3V - development
-// cda-dispe-ElasticL-1XM2UQU1NXKT6 - production
-// cda-dispe-ElasticL-1TPFRAVFTJUDW - production
 
-const instance_names = utils.getInstanceNames(classic_elb, [
-    'cda-dispe-ElasticL-1N0VM8V2BCD3V'
+const prod_info = utils.getInstanceInfo(classic_elb, [
+    prod_blue,
+    prod_green
 ]);
 
-const rv = instance_names.then(names => utils.getRegistrationParams('cda-dispe-ElasticL-1N0VM8V2BCD3V', names));
+const dev_info = utils.getInstanceInfo(classic_elb, [
+    dev
+]);
+
+const rv = prod_info.then(info => utils.getRegistrationParams(prod_green, info));
 
 rv.then(console.log);
 
-// Try to deregister in dev env
-// const dereg = utils.deRegisterInstances(classic_elb, 'cda-dispe-ElasticL-1N0VM8V2BCD3V');
-//
-// dereg.then(console.log).catch(console.error);
+// Try to register in dev env
+const reg = utils.registerInstances(classic_elb, dev);
+
+reg.then(console.log).catch(console.error);
