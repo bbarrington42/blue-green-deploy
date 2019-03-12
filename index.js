dcce9145b1a774eb6f9a7c9f2b90d2ecd6ef8c8b
Bill Barrington <bill.barrington@candidpartners.com>
2019-03-12 13:14:06 -0400

    Got prodColor working.

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 'use strict';
 
-require('dotenv').config();
 const utils = require('./lib/utils');
-
-const ELB = require('aws-sdk/clients/elb');
-
-const classic_elb = new ELB({
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
-    region: process.env.region
-});
 
 const dispenser_dev = 'cda-dispe-ElasticL-1N0VM8V2BCD3V';
 const dispenser_prod_blue = 'cda-dispe-ElasticL-1TPFRAVFTJUDW';
@@ -30,5 +21,6 @@ const consumer = 'i-0c5fdd7fe746e13ae';
 
 //utils.setInstances(classic_elb, dispenser_dev, [dispenser]).then(console.log);
 
-utils.inventory(classic_elb, [dispenser_prod_blue, dispenser_prod_green]).then(console.log).catch(console.error);
+utils.inventory([dispenser_prod_blue, dispenser_prod_green]).then(console.log).catch(console.error);
 
+utils.prodColor().then(console.log).catch(console.error);
