a1752b6aaecc87fc2c3ed5cd2931318c45443a34
Bill Barrington <bill.barrington@candidpartners.com>
2019-03-14 20:58:51 -0400

    Removed a bunch of commented out lines in index.js (for POC).

--- index.js
+++ index.js
@@ -2,38 +2,5 @@
 
 const utils = require('./lib/utils');
 
-const dispenser_dev = 'cda-dispe-ElasticL-1N0VM8V2BCD3V';
-const dispenser_prod_blue = 'cda-dispe-ElasticL-1TPFRAVFTJUDW';
-const dispenser_prod_green = 'cda-dispe-ElasticL-1XM2UQU1NXKT6';
-
-
-// const rv = instance_info.then(info => utils.getRegistrationParams(prod_green, info));
-//
-// rv.then(console.log);
-
-// Try to register in dev env
-// const reg = instance_info.then(info => utils.getRegistrationParams(dev, info)).then(params => utils.registerInstances(classic_elb, params));
-//
-// reg.then(console.log).catch(console.error);
-
-const dispenser = 'i-08b6c9c1e1a662382';
-const consumer = 'i-0c5fdd7fe746e13ae';
-
-//utils.setInstances(classic_elb, dispenser_dev, [dispenser]).then(console.log);
-
-//utils.inventory([dispenser_prod_blue, dispenser_prod_green]).then(console.log).catch(console.error);
-
-//utils.prodColor().then(console.log).catch(console.error);
-
-// utils.listRecords().then(console.log);
-//utils.loadBalancers().then(console.log);
-//utils.listRecords().then(console.log);
-
-//utils.describeELBs().then(desc => console.log(JSON.stringify(desc)));
-
-//utils.describeTags([
-//     'cda-consu-ElasticL-BK5LOE3X0J1A',
-//     'cda-dispe-ElasticL-1JO18D9SWAYHG']).then(desc => console.log(JSON.stringify(desc)));
-
 utils.prodColor().then(console.log);
 utils.prodELBs().then(tags => console.log(JSON.stringify(tags))).catch(console.error);
