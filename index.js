1731557e73439242ec9ed794b9a8e04ae61a587d
Bill Barrington <bill.barrington@candidpartners.com>
2019-03-15 08:44:38 -0400

    Checkpoint. Now creating the parameters to the 'setInstances' call.

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 
 const utils = require('./lib/utils');
 
-utils.prodColor().then(console.log);
-utils.prodELBs().then(tags => console.log(JSON.stringify(tags))).catch(console.error);
+// utils.prodColor().then(console.log);
+//utils.prodELBs().then(tags => console.log(JSON.stringify(tags))).catch(console.error);
+
+
+utils.swapInstances().then(v => console.log(JSON.stringify(v))).catch(console.error);
